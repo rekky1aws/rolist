a24854a1f43d7d8499b582a8a42758e03315bbe9
rekky1aws <killikomfr@gmail.com>
2022-08-07 04:17:21 +0200

    About

---
+++ about/index.php
@@ -0,0 +1,33 @@
+ <?php 
+	$DIR = ".."; // Must take to root.
+	require $DIR."/components/head.php";
+?>
+	<link rel="stylesheet" type="text/css" href="/style/about.css">
+	<!-- <script type="text/javascript" src="script/XXXX.js" defer></script>-->
+	<title>Rollist - About Us</title>
+<?php
+	require $DIR."/components/header.php";
+?>
+
+<!-- Page Content -->
+	<div class="container">
+		<section>
+			<h2>Présentation</h2>
+			<p>Une équipe de deux amis développeurs qui se lancent le projet de faire une application Web permettant de suivre et d'organiser leurs parties de Jeu de Rôles sur table. </p>
+		</section>
+
+		<section>
+			<h2>Equipe</h2>
+			<p>L'équipe est composé de Donatien "rekkylaws" Hochart et de Loïc "lolocontact" Petitprez. Nous touchons tous les deux un peu a à tous les aspect de la création de ce site web, du code pur et dur des fonctionnalités, aux aspects estéthiques. </p>
+			<!-- <span>Donatien Hochart :</span> 
+			<ul>
+				<li><a href="https://github.com/rekky1aws">GitHub</a></li>
+			</ul> -->
+		</section>
+	</div>
+
+	Pour l'instant le design est immonde, ca va changer par la suite.
+
+<?php
+	require $DIR."/components/footer.php";
+?>
