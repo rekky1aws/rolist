1043e9c5996149cb562ea1771bbf47e03985469f
= <killikomfr@gmail.com>
2022-09-09 11:29:27 +0200

    Burger Menu Initialization

--- components/head.php
+++ components/head.php
@@ -4,3 +4,5 @@
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="stylesheet" type="text/css" href="/dist/style/main.css">
+	<link rel="stylesheet" type="text/css" href="/dist/style/burger-menu.css">
+	<script type="text/javascript" src="/src/script/burger-menu.js" defer></script>
--- components/header.php
+++ components/header.php
@@ -2,5 +2,15 @@
 <body>
 	<header>
 		<h1> Rollist </h1>
+		<div class="burger-container cliquable">
+			<div class="burger-part"></div>
+			<div class="burger-part"></div>
+			<div class="burger-part"></div>
+		</div>
 	</header>
+	<nav class="burger-menu">
+		<div class="burger-header">
+			
+		</div>
+	</nav>
 	<main>
