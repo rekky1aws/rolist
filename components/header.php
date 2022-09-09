89b8b04968075309bc07342577a05290dffb1652
= <killikomfr@gmail.com>
2022-09-09 12:19:40 +0200

    Added a cross and animation to the burger menu

--- components/header.php
+++ components/header.php
@@ -10,10 +10,16 @@
 	</header>
 	<nav class="burger-menu">
 		<div class="burger-header close-burger">
-			
+			<h2>Menu</h2>
+			<div class="cross-container">
+				<div class="cross">
+					<div class="cross-part"></div>
+					<div class="cross-part"></div>
+				</div>
+			</div>
 		</div>
 		<div class="burger-content">
-			<a href="/about">A propos</a>
+			<a class="cliquable" href="/about">A propos</a>
 		</div>
 	</nav>
 	<main>
