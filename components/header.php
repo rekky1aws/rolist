34dc0ef32e4f47f5f4e06cd0222af5dcd7e2cb88
= <killikomfr@gmail.com>
2022-09-09 11:37:40 +0200

    Burger Menu animation display.

--- components/header.php
+++ components/header.php
@@ -2,14 +2,14 @@
 <body>
 	<header>
 		<h1> Rollist </h1>
-		<div class="burger-container cliquable">
+		<div class="burger-container cliquable open-burger">
 			<div class="burger-part"></div>
 			<div class="burger-part"></div>
 			<div class="burger-part"></div>
 		</div>
 	</header>
 	<nav class="burger-menu">
-		<div class="burger-header">
+		<div class="burger-header close-burger">
 			
 		</div>
 	</nav>
